79709b5acbca99ab8237c4e0d3e8372266a87e13
ReyanAndrea <reyanandre@gmail.com>
2026-05-16 22:15:41 +0700

    nambah komponen navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
